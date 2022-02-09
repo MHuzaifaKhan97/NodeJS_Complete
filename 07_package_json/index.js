e6372aacc0311680f25a878f56554dc200938c84
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-02-10 01:10:00 +0500

    07) Package.json details

---
+++ 07_package_json/index.js
@@ -0,0 +1,16 @@
+// Package.json maintain project details
+// - Project Version
+// - Project Name
+// - Project Repository
+// - Commands used in that project
+// - List of packages depend on the project
+
+
+// Creating Package.json
+
+// - npm init (on Terminal)
+// - npm init --yes (on Terminal)
+
+const colors = require('colors')
+console.log("Hello".green)
+console.log("Huzaifa Khan".red)
