065b5a709d574d1ebce33d053b0cae0703871df1
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-02-19 20:28:54 +0500

    10) Node Simle API.

---
+++ 10_node_simple_api/data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:'Huzaifa Khan', email:'Huzaifa.khan@gmail.com'},
+    {name:'Waleed Khan', email:'Waleed.khan@gmail.com'},
+    {name:'Abubakar Khan', email:'Abubakar.khan@gmail.com'},
+];
+module.exports = data;
