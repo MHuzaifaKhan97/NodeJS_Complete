78d988eecf7f5475be9fffe824337ff6e0c2856b
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-02-20 03:07:17 +0500

    16) NodeJS Work Process

---
+++ 16_nodejs_work_process/index.js
@@ -0,0 +1,13 @@
+console.log("starting")
+
+setTimeout(() => {
+    console.log("2 Second Log")
+}, 2000)
+
+
+setTimeout(() => {
+    console.log("0 Second Log")
+}, 0)
+
+
+console.log("finishing")
