76f6d6b53706f2d96a22273f863f5b87dd22d098
Muhammad Huzaifa Khan <Huzaifa.Khan@tpsonline.com>
2022-02-26 23:20:46 +0500

    Installation of MongoDB

---
+++ 25_download_and_install_mongoDb/index.js
@@ -0,0 +1,8 @@
+// Download MongoDB
+// Install MongoDB
+// Set Environment for MongoDB
+// MongoDB Compass tool
+
+// Download and Install mongodb complete
+// Set environment variable for mongo
+// => C:\Program Files\MongoDB\Server\5.0\bin
