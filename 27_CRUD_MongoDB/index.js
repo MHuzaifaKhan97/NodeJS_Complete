fe23b00fed5f756d0d0cc3fd3a2caa1da9473d60
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-02-27 00:00:57 +0500

    27) CRUD with Mongo DB

---
+++ 27_CRUD_MongoDB/index.js
@@ -0,0 +1,19 @@
+// MongoDB CRUD Operations using CLI 
+
+// Create Database 
+// use e-commerce
+
+//Create Collection
+// db.createCollection('products')
+
+// Insert into Collection
+// db.products.insertOne({name:"Iphone 13", brand:"Apple",price:1000,catecory:"mobile"})
+
+// Get Data
+// db.products.find()
+
+//Update Data
+// db.products.updateOne({name:"A35"},{$set:{brand:"SAMSUNG"}})
+
+//Delete Data
+// db.products.deleteOne({name:'A35'})
