7de97363516f54483384907a80328ca69bc50e91
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-02-26 23:44:13 +0500

    26) MongoDB Basics

---
+++ 26_mongoDb_basic/index.js
@@ -0,0 +1,11 @@
+// What is mongoDB?
+// MongoDB vs SQL
+// MongoDB Basic Commands.
+
+// Some basic commands:
+// - show dbs
+// - use databaseName   => create new database
+// - db.createCollection('collectionName') => create new collection
+// - show collections  => to see collections list
+// - db.collectionName.drop() => delete collection
+// - db.dropDatabase() => delete database
